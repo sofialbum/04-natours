4beb092605a443b7f449867babf9c764a1dad433
Sofía González <sofia_1.6@hotmail.com>
2024-01-13 01:04:40 -0300

    crudoperations

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
+/* eslint-disable import/no-extraneous-dependencies */
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(`Database connected successfully`));
 
 const port = process.env.PORT;
 app.listen(port, () => {
