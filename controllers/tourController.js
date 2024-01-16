36d2133382d5540b6ede658b5da3d45c85503fe6
Sofía González <sofia_1.6@hotmail.com>
2024-01-16 00:26:16 -0300

    advanced_filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,7 +2,7 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    console.log(req.query);
+    console.log(req.query); 
 
     // BUILD QUERY
     // 1) Filtering
@@ -11,14 +11,11 @@ exports.getAllTours = async (req, res) => {
     excludedFields.forEach((el) => delete queryObj[el]);
 
     // 2) Advanced filtering
-    const queryStr = JSON.stringify(queryObj);
-    queryStr.replace(/()/);
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    // { difficulty: 'easy', duration: { $gte: 5 } }
-    // { difficulty: 'easy', duration: { gte: '5' } }
-    // gte. gt, lte, lt
-
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
 
     // EXECUTE QUERY
     const tours = await query;
